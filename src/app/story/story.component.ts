ca8a9c55384e967207214de466d35fb65ba69113
Darmen96 <67601251+Darmen96@users.noreply.github.com>
2021-03-06 06:11:12 +0600

    Individual work, Routing,Directives and Pipes.

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./story.component.css']
 })
 export class StoryComponent implements OnInit {
-  title = 'Story';
+  posts = [
+    {img: 'https://krimzdorovie.ru/media/k2/galleries/415_m/bigstock-Girl-at-dentist-in-dentist-cli-118390988.jpg', name: 'Dental treatment and removal', title: 'Who can get dental services for free and how?'},
+    {img: 'https://avatars.mds.yandex.net/get-zen_doc/1718701/pub_5d98707ba06eaf00aeaab03b_5d9a067b4e057700ae87a8cd/scale_1200', name: 'State employees salary increase', title: 'How much will teachers, doctors and social workers receive?'},
+    {img: 'https://informburo.kz/storage/photos/43/main/C7L1YWG70dY9foZWP85S9Ro2LgHnEyYEyO4FB5j3.jpg', name: 'Investments available to everyone', title: 'How to start investing with $ 10 in the Tabys app'}
+  ];
   constructor() {
   }
   ngOnInit(): void {
