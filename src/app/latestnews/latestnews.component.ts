773e64463a37824e8c38eb0a219b0b3e192d6668
Darmen96 <67601251+Darmen96@users.noreply.github.com>
2021-05-17 04:24:58 +0600

    Angular Project

--- src/app/latestnews/latestnews.component.ts
+++ src/app/latestnews/latestnews.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NewsService} from '../services/news.service';
 
 @Component({
   selector: 'app-latestnews',
@@ -8,15 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class LatestnewsComponent implements OnInit {
   title = 'Latest News';
   hText = 'Current informations in our country and cities.';
-  newsItems = [
-    {img: 'https://odessa.web2ua.com/wp-content/uploads/2020/05/562235_v_odesskoj_oblasti_za_sutki_vyzdorovelo_r.jpeg', title: 'Infected with covid', content: 'During the day, 766 cases of Covid-19 infection were detected in Kz'},
-    {img: 'https://www.1zoom.ru/big2/47/234125-svetik.jpg', title: 'Weather forecast for February 21', content: 'A low-level snowstorm is expected in the north of Kz, in the south – fog, ice'},
-    // tslint:disable-next-line:max-line-length
-    {img: 'https://informburo.kz/storage/photos/123/main/tOgunBhkuIT2XEnQCpDccwefuH5rwclqsk8xGX68.jpg', title: 'Stanislav Chan, Home Credit Bank. ', content: 'All mobile applications will be integrated into ecosystems'},
-  ];
-  constructor() { }
+  Post: any = [];
+
+  constructor(public restApi: NewsService) { }
 
   ngOnInit(): void {
+    this.loadNeews();
   }
 
+  // tslint:disable-next-line:typedef
+  loadNeews(){
+    return this.restApi.getNeews().subscribe((data: {}) => {
+      this.Post = data;
+      console.log(data);
+    });
+  }
 }
